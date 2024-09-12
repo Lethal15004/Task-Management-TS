4073146225534e0425107c9f84813781de61dd23
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-12 16:38:48 +0700

    Phân trang

--- controller/client/task.controller.ts
+++ controller/client/task.controller.ts
@@ -12,7 +12,18 @@ export const index=async(req:Request,res:Response)=>{
     if(req.query.sortKey && req.query.sortValue){
         sort[`${req.query.sortKey}`]=req.query.sortValue;
     }
-    const tasks = await Task.find(find).sort(sort);
+    const pagination={
+        currentPage:1
+    }
+    if(req.query.limitItems){
+        pagination['limitItems']=parseInt(`${req.query.limitItems}`);
+    }
+    if(req.query.page){
+        pagination.currentPage=parseInt(`${req.query.page}`);
+    }
+    pagination['skip']=(pagination.currentPage-1)*pagination['limitItems'];
+
+    const tasks = await Task.find(find).limit(pagination['limitItems']).skip(pagination['skip']).sort(sort);
     res.json(tasks);
 }
 export const detail=async(req:Request,res:Response)=>{
